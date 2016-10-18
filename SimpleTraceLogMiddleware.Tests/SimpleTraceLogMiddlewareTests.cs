c865bb1b35eab2906e1326ee9747cf13dd9bbc1d
Matthijs Breemans <m.breemans@rubicon.nl>
2016-10-18 13:40:16 +0200

    initial commit

---
+++ SimpleTraceLogMiddleware.Tests/SimpleTraceLogMiddlewareTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Owin.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Owin;
+
+using SimpleTraceLogMiddleware;
+
+namespace SimpleTraceLogMiddleware.Tests
+{
+    [TestClass]
+    public class SimpleTraceLogMiddlewareTests
+    {
+
+        [TestMethod]
+        public async Task OwinAppTest()
+        {
+            using (var server = TestServer.Create<TestStartup>())
+            {
+                var listener = new DebugTraceListener();
+
+                //clear trace listeners and add our own
+                Trace.Listeners.Clear();
+                Trace.Listeners.Add(listener);
+
+                //do request
+                var response = await server.HttpClient.GetAsync("/");
+
+                //check if our tracelistener has the correct value
+                Assert.AreEqual<string>(listener.Value, "ERROR 500 - Internal server error");
+            }
+        }
+
+
+        public class TestStartup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                app.UseSimpleTraceLogMiddleware();
+                app.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    context.Response.ReasonPhrase = "Internal server error";
+                    await context.Response.WriteAsync("Hello world using OWIN TestServer");
+
+                });
+                
+            }
+        }
+    }
+
+    public class DebugTraceListener : TraceListener
+    {
+        public string Value { get; set; }
+        public DebugTraceListener()
+        {
+            this.Name = "Testing Trace Listener";
+        }
+
+        public DebugTraceListener(string name)
+            : base(name)
+        {
+        }
+
+        public override void Write(string message)
+        {
+
+        }
+
+        public override void WriteLine(string message)
+        {
+            Value = message;
+        }
+        public override void Fail(string message, string detailMessage)
+        {
+        }
+    }
+}
---
+++ SimpleTraceLogMiddleware/SimpleTraceLogMiddleware.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Owin;
+
+namespace SimpleTraceLogMiddleware
+{
+    /// <summary>
+    /// Extreme simple webapi response trace logger
+    /// </summary>
+    public class SimpleTraceLogMiddleware
+    {
+        /// <summary>
+        /// Next method function
+        /// </summary>
+        private readonly Func<IDictionary<string, object>, Task> nextFunc;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="nextFunc">Next function to invoke</param>
+        public SimpleTraceLogMiddleware(Func<IDictionary<string, object>, Task> nextFunc)
+        {
+            this.nextFunc = nextFunc;
+        }
+
+        /// <summary>
+        /// This is where it happens
+        /// </summary>
+        /// <param name="env">Environment parameters</param>
+        /// <returns></returns>
+        public async Task Invoke(IDictionary<string, object> env)
+        {
+            //First, call the next function
+            await nextFunc.Invoke(env);
+
+            //Parse response
+            object responseStatusCode;
+            object responseReasonPhrase;
+
+            //quick way to fix OWIN's header's mismatch
+            if (env.TryGetValue(env.Keys.First(x => x.Contains("ResponseStatusCode")), out responseStatusCode) &&
+                env.TryGetValue(env.Keys.First(x => x.Contains("ResponseReasonPhrase")), out responseReasonPhrase))
+            {
+                if (responseStatusCode != null)
+                {
+                    if (responseStatusCode.ToString().StartsWith("5"))
+                    {
+                        Trace.TraceError("ERROR " + responseStatusCode + " - " + responseReasonPhrase);
+                    }
+                    else if (responseStatusCode.ToString().StartsWith("4") || responseStatusCode.ToString().StartsWith("4"))
+                    {
+                        Trace.TraceWarning("WARN " + responseStatusCode + " - " + responseReasonPhrase);
+                    }
+                    else
+                    {
+                        Trace.TraceInformation("INFO " + responseStatusCode + " - " + responseReasonPhrase);
+                    }
+
+                }
+            }
+
+        }
+    }
+    /// <summary>
+    /// Static class for the middleware extension
+    /// </summary>
+    public static class SimpleTraceLogMiddlewareExtension
+    {
+        /// <summary>
+        /// Extension method
+        /// </summary>
+        /// <param name="app">Owin app builder</param>
+        /// <returns>App builder</returns>
+        public static IAppBuilder UseSimpleTraceLogMiddleware(this IAppBuilder app)
+        {
+            return app.Use(typeof(SimpleTraceLogMiddleware));
+        }
+    }
+}
